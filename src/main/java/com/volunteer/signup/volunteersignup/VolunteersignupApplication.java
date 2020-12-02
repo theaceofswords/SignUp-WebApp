66f7a82c972f07b88661fba7efd5883ac042ba29
navaneeth.k <navaneeth.k@qburst.com>
2020-12-02 22:19:25 +0530

    Added HTML and CSS

---
+++ src/main/java/com/volunteer/signup/volunteersignup/VolunteersignupApplication.java
@@ -0,0 +1,13 @@
+package com.volunteer.signup.volunteersignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteersignupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteersignupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volunteer/signup/volunteersignup/VolunteersignupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunteer.signup.volunteersignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteersignupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
